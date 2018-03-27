e8b6fae2b942130979eab16d53548de33a838b74
JeremShy <jcamhi@student.42.fr>
2018-03-28 00:14:23 +0200

    Rainbow Dash is best pony !

--- srcs/icmp.c
+++ srcs/icmp.c
@@ -1,17 +1,15 @@
-#include <ft_traceroute.h>
+=#include <ft_traceroute.h>
 
 void	send_icmp_packet(t_data *data, uint8_t ttl)
 {
 	char			dgram[100];
 	struct icmphdr	*icmp_header;
-	struct timeval	*tv;
 	uint16_t		*seq;
 
 	setsockopt(data->sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
 	ft_bzero(dgram, sizeof(dgram));
 	icmp_header = (void*)dgram;
 	seq = (void*)dgram + sizeof(struct icmphdr);
-	tv = (void*)dgram + sizeof(struct icmphdr) + sizeof(uint16_t);
 
 	icmp_header->type = ICMP_ECHO;
 	icmp_header->code = 0;
@@ -19,7 +17,7 @@ void	send_icmp_packet(t_data *data, uint8_t ttl)
 	icmp_header->un.echo.sequence = htons(data->seq);
 	*seq = (uint16_t)data->seq;
 	*seq = 0xDEAD;
-	gettimeofday(tv, NULL);
+	gettimeofday(&data->array[data->seq], NULL);
 	data->seq++;
 	icmp_header->checksum = checksum(dgram, sizeof(dgram));
 	sendto(data->sock, dgram, sizeof(dgram), 0, data->res->ai_addr, data->res->ai_addrlen);
--- srcs/udp.c
+++ srcs/udp.c
@@ -3,18 +3,16 @@
 void	send_udp_packet(t_data *data, uint8_t ttl)
 {
 	char			dgram[60];
-	struct timeval	*tv;
 	uint16_t		*seq;
 
 	setsockopt(data->sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
 	ft_bzero(dgram, sizeof(dgram));
 	seq = (void*)dgram;
-	tv = (void*)dgram + sizeof(uint16_t);
 
 	*seq = (uint16_t)data->seq;
 	((struct sockaddr_in*)data->res->ai_addr)->sin_port = htons(33434 + data->seq);
+	gettimeofday(&data->array[data->seq], NULL);
 	data->seq++;
-	gettimeofday(tv, NULL);
 	if (sendto(data->sock, dgram, sizeof(dgram), 0, data->res->ai_addr, data->res->ai_addrlen) != sizeof(dgram))
 	{
 		dprintf(2, "Write error.\n");
